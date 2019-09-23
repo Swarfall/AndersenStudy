edf628a66ef7dd0f5defc87e1e24dd878d5a051f
Viacheslav <savitsky,fit@gmail.com>
2019-09-23 15:49:27 +0300

    Complete task 23.09.2019

--- AndersenTest/AndersenTest/Constraint/AnimationViewController.swift
+++ AndersenTest/AndersenTest/Constraint/AnimationViewController.swift
@@ -16,30 +16,14 @@ class AnimationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layoutIfNeeded()
-        createGreenViewConstraint()
+     
     }
     
-    func createGreenViewConstraint() {
-        greenView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
-        greenView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/2).isActive = true
-        greenView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        greenView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    }
     
     @IBAction func didTapAnimateViewButton(_ sender: UIButton) {
-//        UIView.animate(withDuration: 3.0, animations: {
-//            //self.widthGreenViewConstraint.priority = UILayoutPriority(1000)
-//            self.greenView.widthAnchor.constraint(equalToConstant: 100)
-//            self.greenView.center = self.view.center
-////            self.greenView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-////            self.greenView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-//        }, completion: nil)
+        widthGreenViewConstraint.constant = 10
         UIView.animate(withDuration: 3.0, animations: {
-            self.greenView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1/2).isActive = false
-            self.greenView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+            self.view.layoutIfNeeded()
         }, completion: nil)
     }
 }
--- AndersenTest/AndersenTest/Constraint/ConstraintViewController.swift
+++ AndersenTest/AndersenTest/Constraint/ConstraintViewController.swift
@@ -10,24 +10,16 @@ import UIKit
 
 class ConstraintViewController: UIViewController {
 
-    let viewGrey: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor.gray
-        return view
-    }()
+//    let viewGrey: UIView = {
+//        let view = UIView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.backgroundColor = UIColor.gray
+//        return view
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(viewGrey)
-        createViewWhiteConstraint()
     }
     
-    func createViewWhiteConstraint() {
-        viewGrey.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
-        viewGrey.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/2).isActive = true
-        viewGrey.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        viewGrey.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    }
 }
